964d046c8ac62fb2dad5e8452f17495b36abb0d9
R0cker77 <3odyhayek@gmail.com>
2022-05-23 21:23:14 +0300

    couple of steps forward ...

--- lexical_analysis.py
+++ lexical_analysis.py
@@ -2,19 +2,19 @@ import sys
 import os
 from keywords import KEYWORDS
 
-token = {
-        "Name": "",
-        "Type": "",
-        "Line No": 0
-    }
+# token = {
+#         "Name": "",
+#         "Type": "",
+#         "Line No": 0
+#     }
 
 
-# def print_token(name, _type, line_number):
-#     token["Name"] = name
-#     token["Type"] = _type
-#     token["Line No"] = line_number
-#     print(token)
-#     print("\n")
+# # def print_token(name, _type, line_number):
+# #     token["Name"] = name
+# #     token["Type"] = _type
+# #     token["Line No"] = line_number
+# #     print(token)
+# #     print("\n")
 
 
 # def lexical_analysis(pascal_file):
@@ -22,15 +22,15 @@ token = {
 #         count_line = 0
 #         for line in file:
 #             count_line += 1
-#             # Remove newline char from  line
+#             # Remove newline word from  line
 #             refined_line = line.strip().replace(' ', '').lower()
 #             buffer = ''
-#             for char in refined_line:
-#                 # keep putting each new char into buffer until a keyword is
-#                 # met, either one char keyword, or multi char keyword
+#             for word in refined_line:
+#                 # keep putting each new word into buffer until a keyword is
+#                 # met, either one word keyword, or multi word keyword
 #                 # if no keyword is met, its either an int or var
-#                 buffer += char
-#                 if char in KEYWORDS:
+#                 buffer += word
+#                 if word in KEYWORDS:
 #                     buffer = buffer[:-1]
 #                     if len(buffer) != 0:
 #                         try:
@@ -39,24 +39,79 @@ token = {
 #                         except ValueError:
 #                             print_token(buffer, '2', count_line)
 #                     buffer = ''
-#                     print_token(char, '0', count_line)
+#                     print_token(word, '0', count_line)
 #                 elif buffer in KEYWORDS:
 #                     print_token(buffer, '0', count_line)
 #                     buffer = ''
 #         buffer = ''
 
-class Analyzer:
-    def __init__(self, pascal_file, symbol_table):
-        with open(pascal_file, 'r+') as file:
-            input_string = file.readlines()
-            self.refined_line = ''.join([line.strip() for line in input_string])
-        
-        self.refined_line = self.refined_line.replace(' ', '').lower()
-    def next_token(self):
-        pass
-               
 
-if __name__ == '__main__':
+class Analyzer:
+    def __init__(self, pascal_file, symbol_table=[]):
+        with open(pascal_file, "r+") as file:
+            self.input_string = file.read()
+            self.input_string.append("@")
+            self.input_string.split("\n")
+
+        self.input_string = [
+            line.replace("  ", " ").strip() for line in self.input_string
+        ]
+        self.symbol_table = symbol_table
+        self.token = {"Name": "", "Type": "", "Line No": 0}
+
+    def put_into_symbol_table(self, name, type_, line):
+        self.token["Name"] = name
+        self.token["Type"] = type_
+        self.token["Line No"] = line
+
+        self.symbol_table.append(self.token)
+
+        self.token = {"Name": "", "Type": "", "Line No": 0}
+
+    def next_token(self):
+        for line_ in self.input_string:
+            line_count = 1
+            for word in line_.split(" "):
+                if word == "":
+                    continue
+
+                buffer = []
+                last_keyword = 0
+                if word in KEYWORDS:
+                    token = self.put_into_symbol_table(word, 0, line_count)
+                    yield token
+                    continue
+                if word.isnumeric():
+                    token = self.put_into_symbol_table(word, 1, line_count)
+                    yield token
+                    continue
+                for char, index in zip(word, range(len(word))):
+                    if char in KEYWORDS:
+                        if last_keyword == 0:
+                            buffer.append(word[:index])
+                            buffer.append(word[index])
+                            last_keyword = index
+                            continue
+                        buffer.append(word[last_keyword + 1 : index])
+                        buffer.append(word[index])
+                        last_keyword = index
+                if last_keyword == 0:
+                    token = self.put_into_symbol_table(word, 2, line_count)
+                    yield token
+                    continue
+                print(buffer)
+                for entry in buffer:
+                    if entry in KEYWORDS:
+                        token = self.put_into_symbol_table(entry, 0, line_count)
+                    if entry.isnumeric():
+                        token = self.put_into_symbol_table(entry, 1, line_count)
+                    else:
+                        token = self.put_into_symbol_table(entry, 2, line_count)
+                    yield token
+            line_count += 1
+
+
+if __name__ == "__main__":
     try:
         file = sys.argv[1]
     except IndexError:
@@ -64,7 +119,9 @@ if __name__ == '__main__':
         sys.exit()
 
     if os.path.exists(file):
-        analyzer = Analyzer(file)
+        symbol_table = []
+        analyzer = Analyzer(file, symbol_table)
+        get_token = analyzer.next_token()
     else:
         print("Cant find file!")
         sys.exit()
---
+++ production_rules.py
@@ -0,0 +1,134 @@
+def header():
+    pass
+
+
+def identifier_list():
+    pass
+
+
+def type_():
+    pass
+
+
+def standard_type():
+    pass
+
+
+def subprogram_declarations():
+    pass
+
+
+def subprogram_declaration():
+    pass
+
+
+def subprogram_head():
+    pass
+
+
+def arguments():
+    pass
+
+
+def parameter_list():
+    pass
+
+
+def compound_statement():
+    pass
+
+
+def optional_statements():
+    pass
+
+
+def statement_list():
+    pass
+
+
+def statement():
+    pass
+
+
+def match_statement():
+    pass
+
+
+def variable():
+    pass
+
+
+def procedure_statement():
+    pass
+
+
+def expression_list():
+    pass
+
+
+def expression():
+    pass
+
+
+def simple_expression():
+    pass
+
+
+def term():
+    pass
+
+
+def factor():
+    pass
+
+
+def sign():
+    pass
+
+
+def identifier_list_():
+    pass
+
+
+def declarations():
+    pass
+
+
+def subprogram_declarations_():
+    pass
+
+
+def parameter_list_():
+    pass
+
+
+def statement_list_():
+    pass
+
+
+def expression_list_():
+    pass
+
+
+def simple_expression_():
+    pass
+
+
+def term_():
+    pass
+
+
+def relop():
+    pass
+
+
+def addop():
+    pass
+
+
+def mulop():
+    pass
+
+
+def assignop():
+    pass
