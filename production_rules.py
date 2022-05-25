9ec637b358b4a7e3d29dcc997309448cb99794c3
R0cker77 <3odyhayek@gmail.com>
2022-05-25 04:32:18 +0300

    muuuuhh

--- production_rules.py
+++ production_rules.py
@@ -1,134 +1,134 @@
-def header():
+def header(production_num):
     pass
 
 
-def identifier_list():
+def identifier_list(production_num):
     pass
 
 
-def type_():
+def type_(production_num):
     pass
 
 
-def standard_type():
+def standard_type(production_num):
     pass
 
 
-def subprogram_declarations():
+def subprogram_declarations(production_num):
     pass
 
 
-def subprogram_declaration():
+def subprogram_declaration(production_num):
     pass
 
 
-def subprogram_head():
+def subprogram_head(production_num):
     pass
 
 
-def arguments():
+def arguments(production_num):
     pass
 
 
-def parameter_list():
+def parameter_list(production_num):
     pass
 
 
-def compound_statement():
+def compound_statement(production_num):
     pass
 
 
-def optional_statements():
+def optional_statements(production_num):
     pass
 
 
-def statement_list():
+def statement_list(production_num):
     pass
 
 
-def statement():
+def statement(production_num):
     pass
 
 
-def match_statement():
+def match_statement(production_num):
     pass
 
 
-def variable():
+def variable(production_num):
     pass
 
 
-def procedure_statement():
+def procedure_statement(production_num):
     pass
 
 
-def expression_list():
+def expression_list(production_num):
     pass
 
 
-def expression():
+def expression(production_num):
     pass
 
 
-def simple_expression():
+def simple_expression(production_num):
     pass
 
 
-def term():
+def term(production_num):
     pass
 
 
-def factor():
+def factor(production_num):
     pass
 
 
-def sign():
+def sign(production_num):
     pass
 
 
-def identifier_list_():
+def identifier_list_(production_num):
     pass
 
 
-def declarations():
+def declarations(production_num):
     pass
 
 
-def subprogram_declarations_():
+def subprogram_declarations_(production_num):
     pass
 
 
-def parameter_list_():
+def parameter_list_(production_num):
     pass
 
 
-def statement_list_():
+def statement_list_(production_num):
     pass
 
 
-def expression_list_():
+def expression_list_(production_num):
     pass
 
 
-def simple_expression_():
+def simple_expression_(production_num):
     pass
 
 
-def term_():
+def term_(production_num):
     pass
 
 
-def relop():
+def relop(production_num):
     pass
 
 
-def addop():
+def addop(production_num):
     pass
 
 
-def mulop():
+def mulop(production_num):
     pass
 
 
-def assignop():
+def assignop(production_num):
     pass
